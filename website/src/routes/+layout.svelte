1fd93a4391c0842027b1ba769b2a24988e585ab3
SitterM <xxv.many@gmail.com>
2023-06-20 15:54:28 +0200

    init

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,40 +1,31 @@
 
-<script>
-    import './styles.css'
-</script>
+
 
 
 <nav class="top-nav">
     <h1>top nav</h1>
+    <button on:click={_toggleDarkMode}>Toggle</button>
+
 </nav>
 
-<div>
-    <h1>i am hidden</h1>
+
+<div class="content">
+    <slot></slot>
 </div>
 
-<slot></slot>
 
 
 <footer class="bottom-footer">
     <h1>footer</h1>
 </footer>
 <style>
-    :global(*){
-        scroll-behavior: smooth;
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
-        overflow: scroll;
-        scrollbar-width: thin;
-        color: white;
-    }
-    :global(body){
-        background-color: #1e1e1e;
+    .content{
+        margin-top: 50px;
     }
 
     .top-nav{
-        background-color: #333;
+        background-color: var(--some-other-value);
+        color: var(--some-value);
         overflow: hidden;
         position: fixed;
         top: 0;
@@ -43,9 +34,9 @@
     }
 
     .bottom-footer{
-        background-color: #333;
+        background-color: var(--some-other-value);
+        color: var(--some-value);
         bottom: 0;
-        color: #fff;
         left: 0;
         padding: 10px;
         position: fixed;
@@ -54,3 +45,8 @@
         z-index: 1;
     }
 </style>
+
+<script>
+    import './styles.css';
+    import {_toggleDarkMode} from "./+layout.ts";
+</script>
---
+++ website/src/routes/+layout.ts
@@ -0,0 +1,19 @@
+// toggle to switch classes between .light and .dark
+// if no class is present (initial state), then assume current state based on system color scheme
+// if system color scheme is not supported, then assume current state is light
+export function _toggleDarkMode() {
+    if (document.documentElement.classList.contains("light")) {
+        document.documentElement.classList.remove("light")
+        document.documentElement.classList.add("dark")
+    } else if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark")
+        document.documentElement.classList.add("light")
+    } else {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+
+            document.documentElement.classList.add("light")
+        } else {
+            document.documentElement.classList.add("dark")
+        }
+    }
+}
