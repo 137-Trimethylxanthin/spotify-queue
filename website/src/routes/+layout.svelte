bdff1fbdbe01606845998df4a01306f8913142f9
SitterM <xxv.many@gmail.com>
2023-06-20 16:17:43 +0200

    init

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -3,8 +3,13 @@
 
 
 <nav class="top-nav">
-    <h1>top nav</h1>
-    <button on:click={_toggleDarkMode}>Toggle</button>
+    <h1 class="left">top nav</h1>
+    <ul>
+        <li><button on:click={_toggleDarkMode}>Toggle</button></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+
 
 </nav>
 
@@ -25,12 +30,28 @@
 
     .top-nav{
         background-color: var(--some-other-value);
-        color: var(--some-value);
         overflow: hidden;
         position: fixed;
         top: 0;
         width: 100%;
         z-index: 1;
+        padding: 10px;
+        display: inline-block;
+        color: var(--some-value);
+    }
+    .top-nav ul{
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+    .top-nav ul li{
+
+        display: inline-block;
+        margin: 0 10px;
+    }
+    .top-nav .left{
+        float: left;
+        margin-right: 10vh;
     }
 
     .bottom-footer{
